1796004eb63e02222e0eaea531138fe4672ff07c
Serban Gherescu <sgherescu@yahoo.com>
2024-01-26 19:58:50 +0200

    Change order in while loop for not displaying starting message again when player pick a wrong number

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -15,9 +15,10 @@ public class Game {
         JOptionPane.showMessageDialog(null, user + " you start with " + points + " points");
 
 //        Here I told to the user the rules of the game
-        JOptionPane.showMessageDialog(null, "RULES : You need to pick a number between 1 and 6 " +
-                "\nIf you get high number than me, you win 10 points, otherwise you lose 5 points. " +
-                "\nHave fun!");
+        JOptionPane.showMessageDialog(null, """
+                RULES : You need to pick a number between 1 and 6\s
+                If you get high number than me, you win 10 points, otherwise you lose 5 points.\s
+                Have fun!""");
 
 //        I created a LOOP for entire game for executing following lines until player lose all points
         while (points > 0) {
@@ -27,21 +28,21 @@ public class Game {
             int playerNumber = Integer.parseInt(JOptionPane
                     .showInputDialog(user + " your points is : " + points + "\n" + "Enter your number"));
 
-//            Here I display his number
-            JOptionPane.showMessageDialog(null, "Your number is :" + playerNumber);
-//            here I create a little of suspense :))
-            JOptionPane.showMessageDialog(null, "My number is ...");
-
-//            Here I created a variable for a random number;
-            Random computerNumber = new Random();
-//            Here I initialize the random number
-            int x = computerNumber.nextInt(6) + 1;
-//            Here I display the random number
-            JOptionPane.showMessageDialog(null, "My number is :" + x);
-
 //            I created an if statement for conditioning the player to pick a number higher than 0 and lower than 7
 //            (between 1 and 6)
             if (0 < playerNumber && playerNumber < 7) {
+                //            Here I display his number
+                JOptionPane.showMessageDialog(null, "Your number is :" + playerNumber);
+//            here I create a little of suspense :))
+                JOptionPane.showMessageDialog(null, "My number is ...");
+
+//            Here I created a variable for a random number;
+                Random computerNumber = new Random();
+//            Here I initialize the random number
+                int x = computerNumber.nextInt(9) + 1;
+//            Here I display the random number
+                JOptionPane.showMessageDialog(null, "My number is :" + x);
+
 //                I created the condition where player will be winner
                 if (x < playerNumber) {
 //                    adding 10 points for winning
